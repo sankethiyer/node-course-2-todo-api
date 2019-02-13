7bb99819e5aded924f82cef74c9a15b69251bb14
S7701057 <sanketh.iyer@saint-gobain.com>
2019-02-13 22:23:05 +0530

    test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,10 +2,13 @@ const expect = require("expect");
 const request = require("supertest");
 var { app } = require("./../server");
 var { Todo } = require("./../models/todo");
+const {ObjectID} = require('mongodb');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'first test'
 }, {
+    _id: new ObjectID(),
     text: 'second test'
 }];
 
@@ -65,6 +68,32 @@ describe("get todos api", () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe("get todos/:id api", () => {
+    it("it should get correct todo", (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it("it should return 404 if obj not found", (done) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it("it should return 404 if obj id is invalid", (done) => {
+        request(app)
+            .get(`/todos/123`)
+            .expect(404)
             .end(done);
     });
 });
