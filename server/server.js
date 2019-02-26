a6a2f6985c3f666b8b04e64a4cd6d40ba00f1a65
iyersanketh@hotmail.com <iyersanketh@hotmail.com>
2019-02-26 23:37:43 +0530

    patch rest api added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
-const { ObjectID } = require('mongodb');
+
 
 var app = express();
 
@@ -63,6 +65,32 @@ app.delete("/todos/:id", (req, res) => {
             res.status(400).send();
         });
     }
+});
+
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        } else {
+            res.send({ todo });
+        }
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
